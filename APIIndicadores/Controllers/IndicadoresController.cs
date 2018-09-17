016c01babbdc75839734aee2189544a9870910ab
Renato Groffe <renato.groffe@yahoo.com.br>
2018-09-16 21:08:12 -0300

    ASP.NET Core 2.1 + Dapper + SQL Server
    ASP.NET Core 2.1 + Dapper + SQL Server

---
+++ APIIndicadores/Controllers/IndicadoresController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+
+namespace APIIndicadores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IndicadoresController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Indicador> Get(
+            [FromServices]IConfiguration configuration)
+        {
+            using (SqlConnection conexao = new SqlConnection(
+                configuration.GetConnectionString("BaseIndicadores")))
+            {
+                return conexao.Query<Indicador>(
+                    "SELECT * FROM dbo.Indicadores");
+            }
+        }
+    }
+}
